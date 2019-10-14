c5f9e72704146f98c336abce0082f524fc24b331
Yohei Yasukawa <yohei@yasslab.jp>
2019-10-14 08:58:26 +0000

    Exec: rails generate controller StaticPages home help

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,9 @@
+class StaticPagesController < ApplicationController
+  def home
+    # => "app/views/#{リソース名}/@{アクション名}.html.erb"
+    # => "app/views/static_pages/home.html.erb"
+  end
+
+  def help
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  # => StaticPages#home
+  
+  get 'static_pages/help'
+  # => StaticPages#help
+  
   root 'application#hello'
 end
