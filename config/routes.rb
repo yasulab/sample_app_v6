7d591c4e4a68621ff9b4cd5fb20c26e9ace89ae5
Yohei Yasukawa <yohei@yasslab.jp>
2019-10-14 09:43:20 +0000

    Finish ch03

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  # root 'application#hello'
+
   get 'static_pages/home'
   # => StaticPages#home
   
   get 'static_pages/help'
   # => StaticPages#help
   
-  root 'application#hello'
+  get 'static_pages/about'
+  
+ 
 end
