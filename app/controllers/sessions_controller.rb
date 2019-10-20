d857440834d7c63de62e89335b70dc78178950e7
Yohei Yasukawa <yohei@yasslab.jp>
2019-10-20 07:49:04 +0000

    Finish ch07

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,33 @@
+class SessionsController < ApplicationController
+  
+  # GET /login
+  def new
+    # x @session = Session.new
+    # o scope: :session + url: login_path
+  end
+  
+  # POST /login
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # Success
+      log_in user
+      redirect_to user
+    else
+      # Failure
+      # alert-danger => 赤色のフラッシュ
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+      # redirect_to vs. render
+      # GET /users/1 => show template
+      #                 render 'new' (0回)
+    end
+  end
+  
+  # DELETE /logout
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+    
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,11 @@ class User < ApplicationRecord
   
   validates :password, presence: true, length: { minimum: 6 }
 
+  def User.digest(string)
+    # 引数に渡された文字列をハッシュ化
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
 end
